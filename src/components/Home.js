46efc4992999fdd18b67b7e48cefc8ac77bc53e8
matthewlazewski <matthew.lazewski@gmail.com>
2020-09-19 18:05:01 -0500

    set up user authentication in react

---
+++ src/components/Home.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {Link} from 'react-router-dom'
+
+const Home = (props) => {
+
+    const handleClick = () => {
+        axios.delete('http://localhost:3001/logout', {withCredentials: true})
+            .then(res => {props.handleLogout()
+                props.history.push('/')
+            })
+            .catch(error => console.log(error))
+    }
+    
+    return (
+        <div>
+        <Link to='/login'>Log In</Link>
+        <br></br>
+        <Link to='/signup'>Sign Up</Link>
+        <br></br>
+        {
+            props.loggedInStatus ? 
+            <Link to='/logout' onClick={handleClick}>Logout</Link> : null
+        }
+        </div>
+    );
+};
+export default Home;
