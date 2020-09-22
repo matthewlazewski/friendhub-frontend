5acf8ed96f59ded398f9ad493849ff58ae5ca04c
matthewlazewski <matthew.lazewski@gmail.com>
2020-09-22 09:45:47 -0500

    added post reducer

---
+++ src/reducers/postReducer.js
@@ -0,0 +1,20 @@
+const postsReducer = (state = { posts: [], loading: false }, action) => {
+    switch(action.type) {
+      case 'LOADING_POSTS':
+        return {
+          ...state,
+          posts: [...state.posts],
+          loading: true
+        }
+      case 'ADD_POSTS':
+        return {
+          ...state,
+          posts: action.posts,
+          loading: false
+        }
+      default:
+        return state;
+    }
+  }
+  
+  export default postsReducer;
